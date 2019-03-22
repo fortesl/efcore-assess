aa39b853991e9114284869eee5fe185540b46127
lfortes <lmlf100@gmail.com>
2019-03-21 19:58:38 -0500

    Add assessmentId to UserRole

--- Fortes.Assess.Domain/UserRole.cs
+++ Fortes.Assess.Domain/UserRole.cs
@@ -4,6 +4,7 @@
     {
         public int UserId { get; set; }
         public int RoleId { get; set; }
+        public int AssessmentId { get; set; }
         public User User { get; set; }
         public Role Role { get; set; }
     }
